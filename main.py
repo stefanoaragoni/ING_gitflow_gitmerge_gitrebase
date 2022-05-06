2c448665f3a052fb3b4a6ae5c05368915ef97151
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-05-06 17:44:00 -0600

    buf fix hotfix 2

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ print("this is a feature")
 print("this is a bug fixed in release branch")
 
 print("this is a bug fixed in hotfix branch")
+
+print("this is a bug fixed in hotfix branch 2")
+
+print("this is a bug fixed in hotfix branch 3")
